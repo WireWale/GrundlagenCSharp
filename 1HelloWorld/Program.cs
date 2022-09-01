18a4e2ef2966c5a708b15bed06e0bfd6bd2db9f8
kristian.s8 <kristian.s8@P-0G-0B005-0396>
2022-09-01 12:02:19 +0200

    Projektdateien hinzufügen.

---
+++ 1HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Human!");
+
+//1
+Console.WriteLine("");
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");

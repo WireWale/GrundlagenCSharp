bec711e161c82b085ea0814865dd3f3df60c323b
Kristian Sept <kristian.s8@gso.schule.koeln>
2022-09-08 12:29:51 +0200

    3.4.8.69.420 version

--- 1HelloWorld/Program.cs
+++ 1HelloWorld/Program.cs
@@ -2,11 +2,16 @@
 Console.WriteLine("Hello, Human!");
 
 //1
-Console.WriteLine("");
-Console.WriteLine(" .-.");
-Console.WriteLine("(o o)");
-Console.WriteLine("| O |");
-Console.WriteLine("|   |");
-Console.WriteLine("'~~~'");
+Console.WriteLine("\n .-.\n(o o)\n| O |\n|   |\n'~~~'\n");
 
-//tewst
+//2
+string Vorname2 = "Kristian";
+string Nachname2 = "Sept";
+string Alter2 = "16";
+Console.WriteLine($"{Vorname2}\n{Nachname2}\n{Alter2}\n");
+
+//3
+Console.WriteLine("Wie heißt du?"); string Vorname3 = Console.ReadLine();
+Console.WriteLine("\nWie alt bist du?"); string Alter3 = Console.ReadLine();
+Console.WriteLine("\nIn welcher Klasse bist du?"); string Klasse3 = Console.ReadLine();
+Console.WriteLine($"\n{Vorname3} ist {Alter3} Jahre alt und besucht die {Klasse3} Klasse des Georg-Simon-Ohm Berufskollegs.");

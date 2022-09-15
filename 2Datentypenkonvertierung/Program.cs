3af854db76b9779512127b994ebf8a90363258ef
Kristian Sept <kristian.s8@gso.schule.koeln>
2022-09-15 13:07:20 +0200

    Aufgabe 2 und 3 gemacht

---
+++ 2Datentypenkonvertierung/Program.cs
@@ -0,0 +1,26 @@
+﻿//1
+Console.WriteLine($"size of short = {sizeof(short)}");
+Console.WriteLine($"size of short: {short.MaxValue}\n");
+
+Console.WriteLine($"size of int = {sizeof(int)}");
+Console.WriteLine($"size of int: {int.MaxValue}\n");
+
+Console.WriteLine($"size of long = {sizeof(long)}");
+Console.WriteLine($"size of long: {long.MaxValue}\n");
+
+Console.WriteLine($"size of float = {sizeof(float)}");
+Console.WriteLine($"size of float: {float.MaxValue}\n");
+
+Console.WriteLine($"size of double = {sizeof(double)}");
+Console.WriteLine($"size of double: {double.MaxValue}\n");
+
+Console.WriteLine($"size of decimal = {sizeof(decimal)}");
+Console.WriteLine($"size of decimal: {decimal.MaxValue}\n");
+
+//2
+string Vorname = "Kristian";
+short Alter = 16;
+bool Frage = false;
+Console.WriteLine($"Vorname: {Vorname}");
+Console.WriteLine($"Alter: {Alter}");
+Console.WriteLine($"Ist C# die erste Programmiersprache? {Frage}");
